eb3afd3e28e215460d924379054ee572f99b2038
Administrator <2>
2019-09-26 20:35:42 +0800

    add a Operation.java

---
+++ src/com/java/Operation.java
@@ -0,0 +1,12 @@
+//Operation.java
+public class Operation{
+	public static void main(String[] args){
+		int sum=0;
+		int i=1;
+		while(i<=100){
+			sum+=i;
+			i++;
+		}
+		System.out.println(sum);
+	}
+}
